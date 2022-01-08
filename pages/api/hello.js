b20f9b4b2ed73eb310b1122686b42fb10bb43acf
Tim Duckett <tim@duckett.de>
2022-01-08 23:08:49 +0100

    WIP on length of daylight change

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -2,13 +2,13 @@
 import { TwitterApi } from 'twitter-api-v2';
 import axios from 'axios';
 const { zonedTimeToUtc, utcToZonedTime, format } = require('date-fns-tz')
-import { intervalToDuration, parseISO } from 'date-fns'
+import { intervalToDuration, parseISO, startOfYesterday, lightFormat, formatDistanceStrict } from 'date-fns'
 
 const client = new TwitterApi({
   appKey: 'kK2Xs6ayhzwqyJn0o5FkjaPEs',
   appSecret: 'w8i0kt6futvKNDAnpWsJdbf5lcZr4xJSaHhkAUYfk7xTys1MUp',
   accessToken: '1479805238282526721-zOjpYUoACpw3dtiOlbfp2nuShyEBfw',
-  //accessSecret: '8HpDJSoFKs8nmFlSoAlZ0NC94QWP4BWJ1UgDaLjniEFZR',
+  accessSecret: '8HpDJSoFKs8nmFlSoAlZ0NC94QWP4BWJ1UgDaLjniEFZR',
 });
 
 async function fetch() {
@@ -44,26 +44,52 @@ function calculateDaylight(sunrise, sunset) {
   
 }
 
-async function parseSunriseData(sunriseData) {
+async function parseSunriseData(todayData, yesterdayData) {
 
-  const sunriseTime = convertUTCDateToLocalDate(sunriseData.data.results.sunrise)
+  const todaySunriseTime = convertUTCDateToLocalDate(todayData.data.results.sunrise)
+  const yesterdaySunriseTime = convertUTCDateToLocalDate(yesterdayData.data.results.sunrise)
   
-  const dayLengthInHours = sunriseData.data.results.day_length / 3600
+  // const daylightDiff = calculateDaylightDiff(todayData.data.results.sunrise, yesterdayData.data.results.sunrise)
   
-  const sunsetTime = convertUTCDateToLocalDate(sunriseData.data.results.sunset)
-
-  const daylightLength = calculateDaylight(sunriseData.data.results.sunrise, sunriseData.data.results.sunset)
-
-  const tweetString = `Today the sun will rise at ${sunriseTime} and set ${daylightLength} later at ${sunsetTime} (posted ${format(new Date(), 'HH:mm:ss')})`
-  console.log(`Tweet: ${tweetString}`)
+  const sunsetTime = convertUTCDateToLocalDate(todayData.data.results.sunset)
+  const daylightLength = calculateDaylight(todayData.data.results.sunrise, todayData.data.results.sunset)
+  const tweetString = `Today in Berlin the sun will rise at ${todaySunriseTime} and set ${daylightLength} later at ${sunsetTime} (posted ${format(new Date(), 'HH:mm:ss')})`
   return tweetString
 }
 
-export default async function handler(req, res) {
+function calculateDaylightDiff(todaySunriseTime, yesterdaySunriseTime) {
+  const todayTime = lightFormat(parseISO(todaySunriseTime), 'HHmmss')
+  const yesterdayTime = lightFormat(parseISO(yesterdaySunriseTime), 'HHmmss')
+
+  const diff = formatDistanceStrict(
+    parseISO(todayTime),
+    parseISO(yesterdayTime)
+  )
+  console.log(`diff: ${diff}`)
+  return diff
+}
+
+async function getToday() {
   const url = "https://api.sunrise-sunset.org/json?lat=52.5170365&lng=13.3888599&formatted=0"
+  return await axios.get(url)
+}
+
+async function getYesterday() {
+  const yesterday = startOfYesterday()
+  const urlDate = lightFormat(yesterday, 'yyyy-MM-dd')
+  const url = `https://api.sunrise-sunset.org/json?lat=52.5170365&lng=13.3888599&formatted=0&date=${urlDate}`
+  return await axios.get(url)
+}
+
+export default async function handler(req, res) {
   
-  axios.get(url).then( sunriseApiResponse => {
-    return parseSunriseData(sunriseApiResponse)
+  var fetchedYesterdayResponse
+
+  getYesterday().then ( yesterdayResponse => {
+    fetchedYesterdayResponse = yesterdayResponse
+    return getToday()
+  }).then( todayResponse => {
+    return parseSunriseData(todayResponse, fetchedYesterdayResponse)
   }).then( tweetContent => {
     return post(tweetContent);
   }).then( postingResult => {
