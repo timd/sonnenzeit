5873c89c72de75da4ec6d56872dabc53e0499f17
Tim Duckett <tim@duckett.de>
2022-01-08 21:46:33 +0100

    Parsing dates 'n' stuff

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,74 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { TwitterApi } from 'twitter-api-v2';
+import axios from 'axios';
+const { zonedTimeToUtc, utcToZonedTime, format } = require('date-fns-tz')
+import { intervalToDuration, parseISO } from 'date-fns'
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+const client = new TwitterApi({
+  appKey: 'kK2Xs6ayhzwqyJn0o5FkjaPEs',
+  appSecret: 'w8i0kt6futvKNDAnpWsJdbf5lcZr4xJSaHhkAUYfk7xTys1MUp',
+  accessToken: '1479805238282526721-zOjpYUoACpw3dtiOlbfp2nuShyEBfw',
+  //accessSecret: '8HpDJSoFKs8nmFlSoAlZ0NC94QWP4BWJ1UgDaLjniEFZR',
+});
+
+async function fetch() {
+  return await client.v2.userTimeline('12');
+};
+
+async function post(payload) {
+  return await client.v2.tweet(payload);
+};
+
+function convertUTCDateToLocalDate(localDate) {
+  const date = new Date(localDate)
+  const timeZone = 'Europe/Berlin'
+  const zonedDate = utcToZonedTime(date, timeZone)
+  const time = format(zonedDate, 'HH:mm:ss')
+  return time
+}
+
+function calculateDaylight(sunrise, sunset) {
+  const interval = intervalToDuration({
+    start: parseISO(sunrise), end: parseISO(sunset)
+  })
+
+  var daylight
+
+  if (interval.minutes == 1) {
+    daylight = `${interval.hours} hours, ${interval.minutes} minute`
+  } else {
+    daylight = `${interval.hours} hours, ${interval.minutes} minutes`
+  }
+
+  return daylight
+  
+}
+
+async function parseSunriseData(sunriseData) {
+
+  const sunriseTime = convertUTCDateToLocalDate(sunriseData.data.results.sunrise)
+  
+  const dayLengthInHours = sunriseData.data.results.day_length / 3600
+  
+  const sunsetTime = convertUTCDateToLocalDate(sunriseData.data.results.sunset)
+
+  const daylightLength = calculateDaylight(sunriseData.data.results.sunrise, sunriseData.data.results.sunset)
+
+  const tweetString = `Today the sun will rise at ${sunriseTime} and set ${daylightLength} later at ${sunsetTime} (posted ${format(new Date(), 'HH:mm:ss')})`
+  console.log(`Tweet: ${tweetString}`)
+  return tweetString
+}
+
+export default async function handler(req, res) {
+  const url = "https://api.sunrise-sunset.org/json?lat=52.5170365&lng=13.3888599&formatted=0"
+  
+  axios.get(url).then( sunriseApiResponse => {
+    return parseSunriseData(sunriseApiResponse)
+  }).then( tweetContent => {
+    return post(tweetContent);
+  }).then( postingResult => {
+    res.status(200).json(postingResult)
+  }).catch( (err) => {
+    res.status(500).json(err)
+  })
 }
--- pages/api/index.js
+++ pages/api/index.js
@@ -1,32 +1,73 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import { TwitterApi } from 'twitter-api-v2';
-
-// const client = new TwitterApi('AAAAAAAAAAAAAAAAAAAAAO%2BPXwEAAAAAhNUwRDZSFsUPrDoyR%2B8zuwlPJu8%3DhBtMzbXEAwR6iekX3Xy4UeIE1eTsOk4VFg7dzSIHESMTHOWlKs');
-// const client = new TwitterApi({ username: 'sonnezeitberlin', password: '!8_EVGEPaitYrATrDABZ6ftrvaH!93Ant.JGbm@-8o6emJsC8z'})
+import axios from 'axios';
+const { zonedTimeToUtc, utcToZonedTime, format } = require('date-fns-tz')
+import { intervalToDuration, parseISO } from 'date-fns'
 
 const client = new TwitterApi({
   appKey: 'kK2Xs6ayhzwqyJn0o5FkjaPEs',
   appSecret: 'w8i0kt6futvKNDAnpWsJdbf5lcZr4xJSaHhkAUYfk7xTys1MUp',
-  // Following access tokens are not required if you are
-  // at part 1 of user-auth process (ask for a request token)
-  // or if you want a app-only client (see below)
   accessToken: '1479805238282526721-zOjpYUoACpw3dtiOlbfp2nuShyEBfw',
   accessSecret: '8HpDJSoFKs8nmFlSoAlZ0NC94QWP4BWJ1UgDaLjniEFZR',
 });
 
-const rwClient = client.readWrite;
-  
 async function fetch() {
   return await client.v2.userTimeline('12');
 };
 
-async function post() {
-  const payload = `Tweet posted at ${Date()}`
+async function post(payload) {
   return await client.v2.tweet(payload);
 };
 
+function convertUTCDateToLocalDate(localDate) {
+  const date = new Date(localDate)
+  const timeZone = 'Europe/Berlin'
+  const zonedDate = utcToZonedTime(date, timeZone)
+  const time = format(zonedDate, 'HH:mm:ss')
+  return time
+}
+
+function calculateDaylight(sunrise, sunset) {
+  const interval = intervalToDuration({
+    start: parseISO(sunrise), end: parseISO(sunset)
+  })
+
+  var daylight
+
+  if (interval.minutes == 1) {
+    daylight = `${interval.hours} hours, ${interval.minutes} minute`
+  } else {
+    daylight = `${interval.hours} hours, ${interval.minutes} minutes`
+  }
+
+  return daylight
+  
+}
+
+async function parseSunriseData(sunriseData) {
+
+  const sunriseTime = convertUTCDateToLocalDate(sunriseData.data.results.sunrise)
+  
+  const dayLengthInHours = sunriseData.data.results.day_length / 3600
+  
+  const sunsetTime = convertUTCDateToLocalDate(sunriseData.data.results.sunset)
+
+  const daylightLength = calculateDaylight(sunriseData.data.results.sunrise, sunriseData.data.results.sunset)
+
+  const tweetString = `Today the sun will rise at ${sunriseTime} and set ${daylightLength} later at ${sunsetTime} (posted ${format(new Date(), 'HH:mm:ss')})`
+  
+  return tweetString
+}
+
 export default async function handler(req, res) {
-  await post().then( (result) => {
-    res.status(200).json(result)
+  const url = "https://api.sunrise-sunset.org/json?lat=52.5170365&lng=13.3888599&formatted=0"
+  
+  axios.get(url).then( sunriseApiResponse => {
+    return parseSunriseData(sunriseApiResponse)
+  }).then( tweetContent => {
+    return post(tweetContent);
+  }).then( postingResult => {
+    res.status(200).json(postingResult)
   }).catch( (err) => {
     res.status(500).json(err)
   })
